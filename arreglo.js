68ea8e32a055b94d8a6a7a4f92cefbbab9f6ec3c
MahonriM <cripto3000z@gmail.com>
2020-05-10 00:04:18 -0500

    Create arreglo.js

---
+++ arreglo.js
@@ -0,0 +1,4 @@
+var arreglo=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+for(let i=0;i<arreglo.length;i++){
+alert(arreglo[i]);  
+}
